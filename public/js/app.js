e4463e6e773fd90c84978365b9639deb81d88ffa
Tony Bodniev <74680049+tonymacarony-code@users.noreply.github.com>
2022-07-23 21:24:23 +0300

    test deploy

--- public/js/app.js
+++ public/js/app.js
@@ -283,7 +283,11 @@ document.addEventListener('DOMContentLoaded', function () {
 /**
  * mix-html-builder temporary fix for images START
  */
-
+document.addEventListener('DOMContentLoaded', function () {
+  var pathName = document.location.pathname;
+  pathName = pathName.substring(0, pathName.lastIndexOf('/') + 1);
+  document.body.innerHTML = document.body.innerHTML.replace(/\/public\//g, pathName);
+});
 /**
  * END
  */
